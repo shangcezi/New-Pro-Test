b308f55878bedb828f04d095ccd897f16164463f
1609865258@qq.com <xiaobu09>
2020-08-26 15:12:40 +0800

    add AObj

---
+++ socketTest/socketTest/AObj.h
@@ -0,0 +1,17 @@
+//
+//  AObj.h
+//  socketTest
+//
+//  Created by apple on 2020/8/26.
+//  Copyright © 2020 apple. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AObj : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ socketTest/socketTest/AObj.m
@@ -0,0 +1,13 @@
+//
+//  AObj.m
+//  socketTest
+//
+//  Created by apple on 2020/8/26.
+//  Copyright © 2020 apple. All rights reserved.
+//
+
+#import "AObj.h"
+
+@implementation AObj
+
+@end
